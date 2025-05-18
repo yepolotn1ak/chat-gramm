2f98e20521c488196551ff4e3c4393ef57b885fa
Yevhenii <polotnyak2004@gmail.com>
2025-05-19 00:44:14 +0300

    websocket fix

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const { WebSocketServer } = require('ws');
 const { Utils } = require('./utils/utils.js');
 const { userRouter } = require('./routes/user.route.js');
 const { roomRouter } = require('./routes/room.route.js');
-const { websocket } = require('./websocket/websocket.js').default;
+const { websocket } = require('./websocket/websocket.js').deffault;
 const { errorMiddleware } = require('./middlewares/errorMiddleware.js');
 
 require('dotenv').config();
