9e09a63574ee8e624a677922c939790b2132eff2
Yevhenii <polotnyak2004@gmail.com>
2025-05-18 21:57:41 +0300

    fix

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -9,6 +9,9 @@ const client = new Sequelize({
   password: process.env.DB_PASSWORD || 'qwerty989898',
   database: process.env.DB_DATABASE || 'postgres',
   dialect: 'postgres',
+  retry: {
+    max: 10, // кількість спроб
+  },
 });
 
 module.exports = { client };
