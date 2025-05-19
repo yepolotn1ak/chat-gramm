72330429a5e95f8d608e01200107dc70f4d67709
Yevhenii <polotnyak2004@gmail.com>
2025-05-19 17:02:19 +0300

    fix

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const client = new Sequelize({
-  host: process.env.DB_HOST || 'chatgramm-postgres',
+  host: process.env.DB_HOST || '13.53.42.178',
   port: process.env.DB_PORT || 5432,
   username: process.env.DB_USER || 'messenger_user',
   password: process.env.DB_PASSWORD || 'qwerty989898',
