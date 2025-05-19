4be0ee6b5af9abe956214ba9f13d39d825516866
Yevhenii <polotnyak2004@gmail.com>
2025-05-19 16:52:06 +0300

    fix

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const client = new Sequelize({
   host: process.env.DB_HOST || 'chatgramm-postgres',
   port: process.env.DB_PORT || 5432,
-  username: process.env.DB_USER || 'postgres',
+  username: process.env.DB_USER || 'messenger_user',
   password: process.env.DB_PASSWORD || 'qwerty989898',
-  database: process.env.DB_DATABASE || 'postgres',
+  database: process.env.DB_DATABASE || 'messenger_db',
   dialect: 'postgres',
   retry: {
     max: 10, // кількість спроб
