2b5a589bc7ff92aa679fd503e6062c8c9a79e994
Yevhenii <polotnyak2004@gmail.com>
2025-05-19 14:11:36 +0300

    fix

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://13.53.42.178:3002/';
-export const API_WEBSOCKET = 'ws://13.53.42.178:3002';
+export const API_URL = 'https://13.53.42.178:4443/';
+export const API_WEBSOCKET = 'wss://13.53.42.178:4443/ws';
