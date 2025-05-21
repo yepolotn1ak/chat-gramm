19e51001d9986fc63567f2fe2e28993ef388ad6c
Yevhenii <polotnyak2004@gmail.com>
2025-05-21 14:56:58 +0300

    fix

--- client/src/service/createSocket.ts
+++ client/src/service/createSocket.ts
@@ -67,9 +67,6 @@ export const createSocket = (socketData: Types.Socket) => {
       break;
     }
     case Types.SocketType.Message: {
-      socket.onmessage = null;
-      socket.onopen = null;
-
       socket.onopen = () => {
         socket.send(
           JSON.stringify({
