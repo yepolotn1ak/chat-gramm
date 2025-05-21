c9ec19ad2319e1e72be388d33c8e4947f562f599
Yevhenii <polotnyak2004@gmail.com>
2025-05-21 17:23:29 +0300

    swith server

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://13.53.42.178:4443/api/';
-export const API_WEBSOCKET = 'wss://13.53.42.178:4443/ws';
+export const API_URL = 'https://56.228.34.219:4443/api/';
+export const API_WEBSOCKET = 'wss://56.228.34.219:4443/ws';
