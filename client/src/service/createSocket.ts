fad7873aea545e4cc451362373c67f1dce424094
Yevhenii <polotnyak2004@gmail.com>
2025-05-21 16:04:46 +0300

    fix

--- client/src/service/createSocket.ts
+++ client/src/service/createSocket.ts
@@ -53,7 +53,6 @@ export const createSocket = (socketData: Types.Socket) => {
               if (selectedRoom?.userId === data.userId) {
                 setSelectedRoom(null);
                 setMessages([]);
-              } else {
                 sessionStorage.clear();
               }
               break;
