e423f675ac9682cb63783c7dec4688e84916d129
Yevhenii <polotnyak2004@gmail.com>
2025-06-19 14:40:00 +0300

    Update URLS

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://16.171.208.173:4443/api/';
-export const API_WEBSOCKET = 'wss://16.171.208.173:4443/ws';
+export const API_URL = 'https://13.48.148.108:4443/api/';
+export const API_WEBSOCKET = 'wss://13.48.148.108:4443/ws';
